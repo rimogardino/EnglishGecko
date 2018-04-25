cb571a0d94977b8fb0a1622527a2f07fee0a9240
rimogardino <tiddley@abv.bg>
2018-04-26 01:54:09 +0300

    Initial commit

---
+++ app/src/main/java/org/labs/musaka/englishgecko/MainActivity.java
@@ -0,0 +1,19 @@
+package org.labs.musaka.englishgecko;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+
+
+}
---
+++ app/src/main/java/org/labs/musaka/englishgecko/WordGroups.java
@@ -0,0 +1,13 @@
+package org.labs.musaka.englishgecko;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WordGroups extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_word_groups);
+    }
+}
